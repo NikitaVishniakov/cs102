77fe4fbdbbe87651c51b125034d87581fc10c9b4
Nikita <vishniakovcz@gmail.com>
2016-09-23 00:58:23 +0300

    Added a posibility of changing shift size

--- lab1/caesar.py
+++ lab1/caesar.py
@@ -1,46 +1,35 @@
-def encrypt_caesar(plaintext):
-    """ 
-    Encrypts plaintext using a Caesar cipher.
-
-    >>> encrypt_caesar("PYTHON")
-    'SBWKRQ'
-    >>> encrypt_caesar("python")
-    'sbwkrq'
-    >>> encrypt_caesar("")
-    ''
-    """
+def encrypt_caesar(plaintext, shift):
     ciphertext = "";
-    error = 0
     for i in plaintext:
-        if (99 < ord(i) + 3 < 123) or (67 < ord(i) +3 < 90):
-            ciphertext += chr(ord(i) + 3)
+        if (96 + shift < ord(i) + shift < 123) or (64 + shift < ord(i) +shift  < 90):
+            ciphertext += chr(ord(i) + shift)
         elif ord(i) == 32:
             ciphertext += chr(32)
         else:
-            ciphertext += chr(ord(i) - 23) 
+            ciphertext += chr(ord(i) - 26 + shift) 
     print(plaintext)
     return ciphertext   
-    
-    # PUT YOUR CODE HERE
-    #return ciphertext
 
-def decrypt_caesar(ciphertext):
+def decrypt_caesar(ciphertext, shift):
     plaintext = ""
     for i in ciphertext:
-        if (93 < ord(i) - 3 < 97) or (62 < ord(i) - 3 < 65):
-            plaintext += chr(ord(i) + 23)
+        if (96 - shift < ord(i) - shift < 97) or (64 - shift < ord(i) - shift < 64):
+            plaintext += chr(ord(i) + 26 - shift)
         elif ord(i) == 32:
             plaintext += chr(32)
         else:
-            plaintext += chr(ord(i) - 3)
+            plaintext += chr(ord(i) - shift)
     print(ciphertext)
     return plaintext
 
 text = input("Enter a word:")
+shift = int(input("Enter the shift:"))
+if shift > 26:
+    shift = shift % 26
 answer = input("You want to decrypt or encrypt the word?(e for encrypt/ d for decrypt: ").lower()
 if answer == "e":
-    print(encrypt_caesar(text))
+    print(encrypt_caesar(text,shift))
 elif answer == "d":
-    print(decrypt_caesar(text))
+    print(decrypt_caesar(text,shift))
 else:
     print ("Please, type the correct letter(e or d)")
