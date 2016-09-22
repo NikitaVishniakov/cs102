aabfd08ef052c57b7e0901ccfb33c1fe7a217208
Nikita <vishniakovcz@gmail.com>
2016-09-22 15:35:30 +0300

    Added a keyword convert function and encrypt func sceleton without conditions

--- lab1/vigenere.py
+++ lab1/vigenere.py
@@ -1,3 +1,9 @@
+def key_convert(text, key):
+    if len(text) > len(key):
+        key = key*(len(text)//len(key))  + key[0:len(text)%len(key)]
+    elif len(text) < len(key):
+        key = key[0:key - len(key)%len(text)]
+    return key
 def encrypt_vigenere(plaintext, keyword):
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,6 +15,12 @@ def encrypt_vigenere(plaintext, keyword):
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
+    ciphertext = ""
+    keyword = key_convert(plaintext, keyword)
+    print(keyword)
+    for i in range(0,len(plaintext)):
+        ciphertext += chr(ord(plaintext[i]) + (ord(keyword[i]) - 97))
+    
     # PUT YOUR CODE HERE
     return ciphertext
 
@@ -26,3 +38,12 @@ def decrypt_vigenere(ciphertext, keyword):
     """
     # PUT YOUR CODE HERE
     return plaintext
+text = input("Enter the word: ")
+key = input("Enter the key: ")
+answer = input("You want to decrypt or encrypt the word?(e for encrypt/ d for decrypt: ").lower()
+if answer == "e":
+    print(encrypt_vigenere(text,key))
+elif answer == "d":
+    print(decrypt_vigenere(text,key))
+else:
+    print ("Please, type the correct letter(e or d)")
