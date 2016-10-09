7ea81ed1464a28e98c549ddb886c3879830886a9
Nikita <vishniakovcz@gmail.com>
2016-10-09 17:40:08 +0300

    Исправил доктесты

--- lab1/caesar.py
+++ lab1/caesar.py
@@ -1,4 +1,14 @@
 def encrypt_caesar(plaintext, shift):
+    """ 
+    Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON", 3)
+    ('PYTHON', 'SBWKRQ')
+    >>> encrypt_caesar("python", 3)
+    ('python', 'sbwkrq')
+    >>> encrypt_caesar("", 3)
+    ('', '')
+    """
     ciphertext = ""
     for i in plaintext:
         if ((96 + shift < ord(i) + shift < 123) or
@@ -8,11 +18,18 @@ def encrypt_caesar(plaintext, shift):
             ciphertext += chr(32)
         else:
             ciphertext += chr(ord(i) - 26 + shift)
-    print(plaintext)
-    return ciphertext
+    return   plaintext, ciphertext
 
 
 def decrypt_caesar(ciphertext, shift):
+    """ 
+    >>> decrypt_caesar("PYTHON", 3)
+    ('SBWKRQ', 'PYTHON')
+    >>> decrypt_caesar("python", 3)
+    ('sbwkrq', 'python')
+    >>> decrypt_caesar("", 3)
+    ('', '')
+    """
     plaintext = ""
     for i in ciphertext:
         if ((96 - shift < ord(i) - shift < 97 + shift) or
@@ -22,8 +39,7 @@ def decrypt_caesar(ciphertext, shift):
             plaintext += chr(32)
         else:
             plaintext += chr(ord(i) - shift)
-    print(ciphertext)
-    return plaintext
+    return ciphertext, plaintext
 
 text = input("Enter a word:")
 shift = int(input("Enter the shift:"))
@@ -32,8 +48,12 @@ if shift > 26:
 answer = input("You want to decrypt or encrypt the word?" +
                "(e for encrypt/ d for decrypt: ").lower()
 if answer == "e":
-    print(encrypt_caesar(text, shift))
+    a, b = encrypt_caesar(text, shift)
+    print(a + "\n" + b)
 elif answer == "d":
-    print(decrypt_caesar(text, shift))
+    a, b = decrypt_caesar(text, shift)
+    print(a + "\n" + b)
 else:
     print("Please, type the correct letter(e or d)")
+
+        
