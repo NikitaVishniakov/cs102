0ae3cdb12f63425d3c2e90b461da27045b6f7631
Nikita <vishniakovcz@gmail.com>
2016-10-09 17:40:34 +0300

    Исправил докстесты

--- lab1/vigenere.py
+++ lab1/vigenere.py
@@ -6,52 +6,50 @@ def key_convert(text, key):
     return key
 def encrypt_vigenere(plaintext, keyword):
     """
-    Encrypts plaintext using a Vigenere cipher.
-
     >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
+    ('AAAAAA', 'PYTHON')
     >>> encrypt_vigenere("python", "a")
-    'python'
+    ('aaaaaa', 'python')
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
+    ('LEMONLEMONLE', 'LXFOPVEFRNHR')
     """
     ciphertext = ""
     keyword = key_convert(plaintext, keyword)
-    print(keyword)
     for i in range(0,len(plaintext)):
-        if (96 < ord(plaintext[i]) < 123 - (ord(keyword[i].lower()) - 97)) or (64 < ord(plaintext[i]) < 91- (ord(keyword[i].lower()) - 97)):
+        if ((96 < ord(plaintext[i]) < 123 - (ord(keyword[i].lower()) - 97)) or
+            (64 < ord(plaintext[i]) < 91- (ord(keyword[i].lower()) - 97))):
             ciphertext += chr(ord(plaintext[i]) + ord(keyword[i].lower()) - 97)
         else:
             ciphertext += chr(64 + (ord(plaintext[i]) - 90) + (ord(keyword[i].lower()) - 97))
-    return ciphertext
+    return keyword, ciphertext
 
 
 def decrypt_vigenere(ciphertext, keyword):
     """
-    Decrypts a ciphertext using a Vigenere cipher.
-    
     >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
+    ('AAAAAA', 'PYTHON')
     >>> decrypt_vigenere("python", "a")
-    'python'
+    ('aaaaaa', 'python')
     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
+     ('LEMONLEMONLE', 'ATTACKATDAWN')
     """
     plaintext = ""
     keyword = key_convert(ciphertext, keyword)
-    print(keyword)
     for i in range(0,len(ciphertext)):
-        if (96 + (ord(keyword[i].lower()) - 97) < ord(ciphertext[i]) < 123) or (64 + (ord(keyword[i].lower()) - 97) < ord(ciphertext[i]) < 91):
+        if ((96 + (ord(keyword[i].lower()) - 97) < ord(ciphertext[i]) < 123) or
+            (64 + (ord(keyword[i].lower()) - 97) < ord(ciphertext[i]) < 91)):
             plaintext += chr(ord(ciphertext[i]) - (ord(keyword[i].lower()) - 97))
         else:
-            plaintext += chr(91 - (65 - ord(ciphertext[i]) + (ord(keyword[i].lower()) - 97))) 
-    return plaintext
+            plaintext += chr(91 - (65 - ord(ciphertext[i]) + (ord(keyword[i].lower()) - 97)))
+    return keyword, plaintext
 text = input("Enter the word: ")
 key = input("Enter the key: ")
 answer = input("You want to decrypt or encrypt the word?(e for encrypt/ d for decrypt: ").lower()
 if answer == "e":
-    print(encrypt_vigenere(text,key))
+    a, b = encrypt_vigenere(text,key)
+    print(a + "\n" + b)
 elif answer == "d":
-    print(decrypt_vigenere(text,key))
+    a, b = decrypt_vigenere(text,key)
+    print(a + "\n" + b)
 else:
     print ("Please, type the correct letter(e or d)")
